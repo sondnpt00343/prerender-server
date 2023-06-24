2f0a2e5f313b309dc9479477176839b91f621ea9
Son Dang <sondang@fullstack.edu.vn>
2023-06-24 16:25:10 +0700

    Enable addMetaTags plugin

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const server = prerender({
 server.use(prerender.sendPrerenderHeader());
 server.use(prerender.browserForceRestart());
 server.use(prerender.blockResources());
-// server.use(prerender.addMetaTags());
+server.use(prerender.addMetaTags());
 server.use(prerender.removeScriptTags());
 server.use(prerender.httpHeaders());
 
