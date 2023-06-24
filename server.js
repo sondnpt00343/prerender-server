46634d93f983202690926486b649f8a755256b5f
Son Dang <sondang@fullstack.edu.vn>
2023-06-24 14:34:13 +0700

    Comment browserForceRestart

--- server.js
+++ server.js
@@ -2,14 +2,15 @@
 const prerender = require('./lib');
 const cache = require('prerender-redis-cache');
 
-var server = prerender();
+const server = prerender();
 
 server.use(prerender.sendPrerenderHeader());
-server.use(prerender.browserForceRestart());
+// server.use(prerender.browserForceRestart());
 // server.use(prerender.blockResources());
-server.use(prerender.addMetaTags());
+// server.use(prerender.addMetaTags());
 server.use(prerender.removeScriptTags());
 server.use(prerender.httpHeaders());
+
 server.use(cache);
 
 server.start();
