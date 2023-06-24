f81a9187fb328cb3cc0d2dc6f899625a090a41ed
Son Dang <sondang@fullstack.edu.vn>
2023-06-24 15:06:17 +0700

    Enable blockResources

--- server.js
+++ server.js
@@ -2,11 +2,19 @@
 const prerender = require('./lib');
 const cache = require('prerender-redis-cache');
 
-const server = prerender();
+const server = prerender({
+    chromeFlags: [
+        "--headless",
+        "--disable-gpu",
+        "--remote-debugging-port=9222",
+        "--hide-scrollbars",
+        "--no-sandbox"
+      ]
+});
 
 server.use(prerender.sendPrerenderHeader());
 // server.use(prerender.browserForceRestart());
-// server.use(prerender.blockResources());
+server.use(prerender.blockResources());
 // server.use(prerender.addMetaTags());
 server.use(prerender.removeScriptTags());
 server.use(prerender.httpHeaders());
