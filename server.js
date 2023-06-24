8f638e44b0b36bfc5878236bfcf713615edf1a6f
Son Dang <sondang@fullstack.edu.vn>
2023-06-24 20:01:55 +0700

    Use prerender from npm

--- server.js
+++ server.js
@@ -1,13 +1,11 @@
 #!/usr/bin/env node
-const prerender = require('./lib');
+const prerender = require('prerender');
 const cache = require('prerender-redis-cache');
 
 const server = prerender();
 
 server.use(prerender.sendPrerenderHeader());
-server.use(prerender.browserForceRestart());
 server.use(prerender.blockResources());
-server.use(prerender.addMetaTags());
 server.use(prerender.removeScriptTags());
 server.use(prerender.httpHeaders());
 
