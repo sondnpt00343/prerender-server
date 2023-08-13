615aeaa78c1cef9fd5b7720929bfe28b5c258e19
Son Dang <sondang@fullstack.edu.vn>
2023-08-13 21:17:01 +0700

    Add CHROME_FLAGS env

--- server.js
+++ server.js
@@ -1,8 +1,17 @@
 #!/usr/bin/env node
-const prerender = require('prerender');
-const cache = require('prerender-redis-cache');
+const prerender = require("prerender");
+const cache = require("prerender-redis-cache");
 
-const server = prerender();
+const server = prerender({
+  chromeFlags: [
+    "--headless",
+    "--disable-gpu",
+    "--remote-debugging-port=9222",
+    "--hide-scrollbars",
+    "--window-size=414,736",
+    ...process.env.CHROME_FLAGS.split(","),
+  ],
+});
 
 server.use(prerender.sendPrerenderHeader());
 server.use(prerender.blockResources());
